5d668323f76b4605f38e7f8c49c4f7f7076e45d7
Alexander Yatkevich <ayatkevich@gmail.com>
2018-11-11 15:53:10 +0300

    add mutliple table test for property infer

--- src/typings-tests/postgremote-client.ts
+++ src/typings-tests/postgremote-client.ts
@@ -2,13 +2,17 @@ import { exec } from 'postgremote/client';
 import { jsql } from 'postgremote/jsql';
 
 (async () => {
-  const MyTable = jsql.table('MyTable', [
+  const MyTable1 = jsql.table('MyTable1', [
     jsql.column('c1', { type: String }),
     jsql.column('c2', { type: String })
   ]);
+  const MyTable2 = jsql.table('MyTable2', [
+    jsql.column('d1', { type: String }),
+    jsql.column('d2', { type: String })
+  ]);
 
   for (const row of await exec(
-    jsql.select([MyTable.c1], { from: [MyTable] })
+    jsql.select([MyTable1.c1], { from: [MyTable1] })
   )) {
     row.c1;
 
@@ -19,7 +23,7 @@ import { jsql } from 'postgremote/jsql';
   }
 
   for (const row of await exec(
-    jsql.select([MyTable.c1, MyTable.c2], { from: [MyTable] })
+    jsql.select([MyTable1.c1, MyTable1.c2], { from: [MyTable1] })
   )) {
     row.c1;
     row.c2;
@@ -29,7 +33,7 @@ import { jsql } from 'postgremote/jsql';
   }
 
   for (const row of await exec(
-    jsql.select([MyTable['*'], MyTable.c1.as('c3')], { from: [MyTable] })
+    jsql.select([MyTable1['*'], MyTable1.c1.as('c3')], { from: [MyTable1] })
   )) {
     row.c1;
     row.c2;
@@ -37,5 +41,16 @@ import { jsql } from 'postgremote/jsql';
 
     // here should be error
     row.thereIsNoSuchAColumnHere;
+  }
+
+  for (const row of await exec(
+    jsql.select([MyTable1['*'], MyTable2['*']], {from: [MyTable1, MyTable2]})
+  )) {
+    row.c1;
+    row.c2;
+    row.d1;
+    row.d2;
+
+    // should be no error
   }
 })();
