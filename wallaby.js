a87346ccdf59f0eaeeb368a774bbe760f4920170
Alex Yatkevich <ayatkevich@gmail.com>
2019-02-17 01:20:42 +0300

    update packages, add and configure dotenv

--- wallaby.js
+++ wallaby.js
@@ -1,34 +1,52 @@
-module.exports = function() {
+module.exports = function(wallaby) {
+  var path = require('path');
+  process.env.NODE_PATH +=
+    path.delimiter +
+    path.join(wallaby.localProjectDir, 'postgremote', 'node_modules') +
+    path.delimiter +
+    path.join(wallaby.localProjectDir, 'designpad', 'node_modules');
+
   return {
     debug: true,
 
     files: [
+      { pattern: '.env', instrument: false },
       { pattern: 'node_modules/@types/**/*', instrument: false },
-      { pattern: 'src/**/*.snap', instrument: false },
+      { pattern: 'packages/**/*.snap', instrument: false },
+      { pattern: 'packages/**/node_modules/**', ignore: true },
       'tsconfig.json',
-      'src/**/*.ts',
-      '!src/**/*.spec.ts'
+      'packages/**/*.ts',
+      '!packages/**/*.spec.ts'
     ],
 
-    tests: ['src/**/*.spec.ts'],
-
-    workers: {
-      initial: 4,
-      regular: 4,
-      restart: true
-    },
+    tests: ['packages/**/*.spec.ts'],
 
     env: {
-      type: 'node',
-      runner: 'node'
+      type: 'node'
     },
 
     testFramework: 'jest',
 
-    setup: function() {
-      process.env.POSTGRES_USER = 'postgremote';
-      process.env.POSTGRES_DB = 'postgremote';
-      process.env.POSTGRES_PASSWORD = '';
+    setup: function(w) {
+      require('dotenv').config();
+
+      let jestConfig = global._modifiedJestConfig;
+      if (!jestConfig) {
+        jestConfig = global._modifiedJestConfig = require('./package.json').jest;
+        const path = require('path');
+        const globby = require('globby');
+
+        jestConfig.moduleNameMapper = globby
+          .sync(path.join(w.projectCacheDir, 'packages/*/package.json'))
+          .reduce((acc, v) => {
+            const packageJsonPath = v;
+            acc[`^${require(packageJsonPath).name}(.*)`] = `${path.dirname(
+              packageJsonPath
+            )}$1`;
+            return acc;
+          }, {});
+      }
+      wallaby.testFramework.configure(jestConfig);
     }
   };
 };
